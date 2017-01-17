b676da7d8221343c619bc6dcd77a8b7510de6fe5
Kevin Baptista <kbaptist@emi.u-bordeaux1.fr>
2017-01-17 11:31:53 +0100

    fill Entity

--- src/NotreJeu/Entity.java
+++ src/NotreJeu/Entity.java
@@ -4,24 +4,28 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+import javax.swing.text.Position;
+
 import gameframework.core.Drawable;
 import gameframework.core.GameEntity;
 import gameframework.core.Overlappable;
 
 public abstract class Entity implements GameEntity, Drawable, Overlappable {
-
+	
+	private int RENDERING_SIZE = 16;
+	private Point position = new Point();
+	
 	@Override
 	public Rectangle getBoundingBox() {
 		// TODO Auto-generated method stub
-		return null;
+		return (new Rectangle(0, 0, RENDERING_SIZE, RENDERING_SIZE));
 	}
 
 	@Override
 	public Point getPosition() {
 		// TODO Auto-generated method stub
-		return null;
+		return position;
 	}
-
 	
 	public abstract void draw(Graphics g);
 
