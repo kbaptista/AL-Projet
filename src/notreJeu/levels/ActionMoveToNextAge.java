812162482043684d8d97e0019c8409eabc3484ab
Kevin Baptista <kbaptist@emi.u-bordeaux1.fr>
2017-01-27 14:55:03 +0100

    ActionMoveToNextAge

---
+++ src/notreJeu/levels/ActionMoveToNextAge.java
@@ -0,0 +1,48 @@
+package notreJeu.levels;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+
+import notreJeu.Team;
+import notreJeu.buttons.Action;
+
+public class ActionMoveToNextAge extends Action {
+
+	public ActionMoveToNextAge(JButton button, AbstractLevelCTF level, Team team) {
+		super(button, level,team);
+		button.setText("Back to the Futur");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
